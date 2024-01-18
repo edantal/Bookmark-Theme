8621f60719aaa339e34da70c696eb0bdc448ef7b
edanTal <edant81@gmail.com>
2024-01-18 20:11:53 +0100

    Add smooth scroll for navigation links

--- public/main.js
+++ public/main.js
@@ -1,24 +1,48 @@
-Menu = (e) => {
-  const list = document.querySelector('ul.nav-list')
+// mobile menu
+toggleMobileMenu = (e) => {
+  const list = document.querySelector('.nav-list')
 
-  e.name === 'menu'
-    ? ((e.name = 'close'),
-      e.classList.add('rotate-180'),
-      e.classList.remove('fa-bars'),
-      e.classList.add('fa-xmark'),
-      list.classList.add('top-[80px]'),
-      list.classList.add('opacity-100'))
-    : ((e.name = 'menu'),
-      e.classList.remove('rotate-180'),
-      e.classList.add('fa-bars'),
-      e.classList.remove('fa-xmark'),
-      list.classList.remove('top-[80px]'),
-      list.classList.remove('opacity-100'))
+  if (e.classList.contains('fa-bars')) {
+    e.classList.add('rotate-180')
+    e.classList.remove('fa-bars')
+    e.classList.add('fa-xmark')
+    list.classList.add('top-[80px]')
+    list.classList.add('opacity-100')
+  } else {
+    e.classList.remove('rotate-180')
+    e.classList.add('fa-bars')
+    e.classList.remove('fa-xmark')
+    list.classList.remove('top-[80px]')
+    list.classList.remove('opacity-100')
+  }
 }
 
-const accordionHeader = document.querySelectorAll('.accordion__header')
+// smooth scroll
+const links = document.querySelectorAll('.nav-link')
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn')
 
-accordionHeader.forEach((accordionHeader) => {
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const href = link.getAttribute('href')
+    const offsetTop = document.querySelector(href).offsetTop
+
+    // toggle back mobile menu
+    if (mobileMenuBtn.classList.contains('fa-xmark')) {
+      toggleMobileMenu(mobileMenuBtn)
+    }
+
+    scroll({
+      top: offsetTop,
+      behavior: 'smooth',
+    })
+  })
+})
+
+// accordion component
+const accordionHeaders = document.querySelectorAll('.accordion__header')
+
+accordionHeaders.forEach((accordionHeader) => {
   accordionHeader.addEventListener('click', (e) => {
     accordionHeader.classList.toggle('active')
     const accordionBody = accordionHeader.nextElementSibling
